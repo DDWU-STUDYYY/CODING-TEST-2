d2b035d46af6043be421ff5f3e8fd41f4fdd2270
kimjiheej <glp0509@naver.com>
2024-04-01 20:05:56 +0900

    KJH_0401

---
+++ src/main/java/org/example/part2/ch01/JH/Algo1543.java
@@ -0,0 +1,36 @@
+package org.example.part2.ch01.JH;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algo1543 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int count = 0;
+        int k = 0;
+
+        while (k < a.length()) {
+            boolean match = true;
+            for (int j = 0; j < b.length(); j++) {
+                if (k + j >= a.length() || b.charAt(j) != a.charAt(k + j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                count++;
+                k += b.length();
+            } else {
+                k++;
+            }
+        }
+        System.out.println(count);
+    }
+    }
+
---
+++ src/main/java/org/example/part2/ch01/JH/Algo1919.java
@@ -0,0 +1,40 @@
+package org.example.part2.ch01.JH;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Algo1919 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        int answer = 0;
+
+        int[] countA = new int[26]; // dared [1,0,0,2 ..]
+        int[] countB = new int[26];
+
+        for(int i=0; i<a.length(); i++){
+            countA[a.charAt(i)-'a']++;
+        }
+
+        for(int i=0; i<b.length(); i++){
+            countB[b.charAt(i)-'a']++;
+        }
+
+        for(int i=0; i<countA.length; i++){
+            if(countA[i] != countB[i]){
+                answer += Math.abs(countA[i] - countB[i]);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
+/*
+ 알파벳 배열 만드는 방법을 잘 생각해보자 !!
+ */
