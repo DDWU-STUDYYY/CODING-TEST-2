a351f2fff5b7c9676f596c95ee484cdea9156059
Parkseojin <seojin5565@gmail.com>
2024-04-02 16:45:15 +0900

    [add] : 1543

---
+++ src/main/java/org/example/part1/ch01/SJ/Main1543.java
@@ -0,0 +1,31 @@
+package org.example.part1.ch01.SJ;
+
+import java.io.*;
+
+public class Main1543 {
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String input = br.readLine();
+    String search = br.readLine();
+
+    int i = 0;
+    int idx = input.indexOf(search);
+    int cnt = 0;
+    // System.out.println("idx: "+ idx);
+
+    while (idx < input.length() && idx != -1) {
+      idx = input.indexOf(search, idx + search.length());
+      // System.out.println("idx: "+ idx);
+      cnt++;
+    }
+
+    bw.write(Integer.toString(cnt));
+
+    br.close();
+    bw.close();
+  }
+}
