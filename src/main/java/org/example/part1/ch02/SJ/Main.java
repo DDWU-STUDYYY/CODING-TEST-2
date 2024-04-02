1aa286d10311de74154be1980092ded8ed1a78f3
Parkseojin <seojin5565@gmail.com>
2024-04-02 18:37:49 +0900

    [add] : 10158

--- src/main/java/org/example/part1/ch02/SJ/Main.java
+++
@@ -1,5 +0,0 @@
-package org.example.part1.ch02.SJ;
-
-public class Main {
-
-}
---
+++ src/main/java/org/example/part1/ch02/SJ/Main10158.java
@@ -0,0 +1,52 @@
+package org.example.part1.ch02.SJ;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main10158 {
+
+  public static void main(String[] args) throws Exception {
+//    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+//    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//    StringTokenizer st;
+//
+//    st = new StringTokenizer(br.readLine());
+//
+//    int w = Integer.parseInt(st.nextToken());
+//    int h = Integer.parseInt(st.nextToken());
+//
+//    st = new StringTokenizer(br.readLine());
+//    int currentW = Integer.parseInt(st.nextToken());
+//    int currentH = Integer.parseInt(st.nextToken());
+//
+//    int t = Integer.parseInt(br.readLine());
+//
+//    int mvW = 1;
+//    int mvH = 1;
+//    for (int i = 0; i < t; i++){
+//
+//      // 윗 벽면
+//      if (currentH == h) {
+//        mvW = -1;
+//        mvH = -1;
+//      } else if(currentW == w){ // 오른쪽 벽면
+//        mvW = -1;
+//        mvH = 1;
+//      } else if(){  // 아래 벽면
+//        mvW = -1;
+//        mvH = 1;
+//      }
+//
+//
+//      // 왼쪽 벽면
+//      if (w + 1 )
+//      w += 1;
+//      h += 1;
+//    }
+
+  }
+
+}
