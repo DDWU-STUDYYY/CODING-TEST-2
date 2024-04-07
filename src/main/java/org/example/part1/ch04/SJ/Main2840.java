4182e2bfbb445a6a0329afd8ee4ebe829dfa1b05
Parkseojin <seojin5565@gmail.com>
2024-04-07 20:56:28 +0900

    [add] : 1730

---
+++ src/main/java/org/example/part1/ch04/SJ/Main2840.java
@@ -0,0 +1,49 @@
+package org.example.part1.ch04.SJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2840 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int size = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(st.nextToken());
+    String[] str = new String[size];
+
+    for (int i = 0; i < str.length; i++) {
+      str[i] = "?";
+    }
+
+    int current = 0;
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int cnt = Integer.parseInt(st.nextToken());
+      String ch = st.nextToken();
+
+      int next = current+ cnt;
+      while (next < size) {
+        next %= N;
+      }
+      current += next;
+
+      if (str[current].isEmpty()) {
+        break;
+      }else {
+        str[current] = ch;
+      }
+
+    }
+    i
+    while ()
+    for (int i = 0; i < str.length; i++) {
+      str[i] = "?";
+    }
+
+  }
+
+}
