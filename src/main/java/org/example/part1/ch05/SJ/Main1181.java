2abc89f9083916f4785563cb7ae693263b09ef73
Parkseojin <seojin5565@gmail.com>
2024-04-11 09:45:38 +0900

    [add] : 1181

---
+++ src/main/java/org/example/part1/ch05/SJ/Main1181.java
@@ -0,0 +1,42 @@
+package org.example.part1.ch05.SJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1181 {
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    Set<String> set = new HashSet<String>();
+
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N; i++) {
+      set.add(br.readLine());
+    }
+
+    ArrayList<String> list = new ArrayList<>(set);
+
+    list.sort(new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        if (o1.length() == o2.length()){
+          return o1.compareTo(o2); // String 타입의 비교
+        }
+        return Integer.compare(o1.length(), o2.length());
+      }
+    });
+
+    for (String word : list) {
+      bw.write(word+ "\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+
+  }
+}
+
