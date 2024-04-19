a7f6ebf8388af042e16185ce039bd3c0777e33a4
Parkseojin <seojin5565@gmail.com>
2024-04-19 11:02:21 +0900

    add : 10816

---
+++ src/main/java/org/example/part1/ch07/SJ/Main2470.java
@@ -0,0 +1,89 @@
+package org.example.part1.ch07.SJ;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main2470 {
+
+  static int MIN = Integer.MAX_VALUE;
+
+  // -6 | -9 -5 -4 -3 -2 -1, 4 // 합이 음수면 오른쪽으로 이동해야함
+  // -9 -5 -4 -3 -2 -1, ,4
+  // 123
+  // 합이 0이 되거나 양수가 되는 시점이 포인트? 음수 -> 양수로 넘어가면 중단해야하고, 음수 값 중에서 가장 작았던 것과 비교해야함
+  private static int findMinSum(ArrayList<Integer> arr, int s, int e, int n) {
+    int value = arr.get(s);
+    int minAbsSum = Math.abs(value + n);
+    while(s <= e) {
+      int m = (s + e) / 2;
+      int sum = arr.get(m) + n;
+      int absSum = Math.abs(sum);
+      // 합의 부호가 바뀌는 시점에서..
+      if (absSum < minAbsSum) {
+        minAbsSum = absSum;
+        value = arr.get(m);
+      }
+
+      if (sum < 0) { // 오른쪽 이동
+        s = m + 1;
+      } else if (sum > 0) { // 양수인 경우. 왼쪽으로 이동해야..?
+        e = m - 1;
+      } else { // 합의 부호가 바뀌는 시점인지
+        return arr.get(m);
+      }
+    }
+
+    return value;
+  }
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    ArrayList<Integer> nums = new ArrayList<>();
+
+    for (int i = 0; i < N; i++) {
+      nums.add(Integer.parseInt(st.nextToken()));
+    }
+
+    // 정렬하고 이분탐색
+    // 자기자신과 다른 애들의 합을 구한다.
+    // 그 중 절댓값이 가장 작은 것을 찾는다.ㅜㅜ
+
+    nums.sort(Comparator.naturalOrder());
+
+    int n1 = 0, n2 = 0;
+    int minSum = Integer.MAX_VALUE;
+    for (int i = 0; i < N - 1; i++) {
+      int pair = findMinSum(nums, i + 1, N - 1, nums.get(i));
+      int abs = Math.abs(pair + nums.get(i));
+
+      if (minSum > abs) {
+        minSum = abs;
+        n1 = nums.get(i);
+        n2 = pair;
+      }
+
+    }
+
+    bw.write(String.valueOf(n1)+" "+String.valueOf(n2));
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+}
